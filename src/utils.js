26d3a252a823a4c5fc4586174595028c7182898c
shivanshkc <shivanshbox@gmail.com>
2020-12-06 15:20:29 +0530

    Added utility methods.

--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,17 @@
-module.exports = {};
+const routeFactory = {};
+
+function handleAPIErr(err) {
+  const customCode = getCustomCode(err);
+  if (!customCode) {
+    console.warn("Unexpected error in Ledgerkeep API call:", err.message);
+    throw err;
+  }
+
+  throw new Error(customCode);
+}
+
+function getCustomCode(err) {
+  return err && err.response && err.response.data && err.response.data.customCode;
+}
+
+module.exports = { routeFactory, handleAPIErr };
