f686873f40309b4a970eeebbc4337ef985198d15
shivanshkc <shivanshbox@gmail.com>
2020-12-06 15:55:42 +0530

    Method signatures added.

--- src/ledgerkeep.js
+++ src/ledgerkeep.js
@@ -1,3 +1,29 @@
-class Ledgerkeep {}
+class Ledgerkeep {
+  constructor(addr) {
+    this.__addr = addr;
+  }
+
+  async getCategoryCount() {}
+
+  async putCategory() {}
+
+  async delCategory() {}
+
+  async getAllCategories() {}
+
+  async getTransactionCount() {}
+
+  async getTransactionSum() {}
+
+  async getTransactionByID() {}
+
+  async patchTransactionByID() {}
+
+  async delTransactionByID() {}
+
+  async getAllTransactions() {}
+
+  async postTransaction() {}
+}
 
 module.exports = Ledgerkeep;
